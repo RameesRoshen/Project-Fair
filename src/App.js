9fc87082e11841f49c291695a1bdd21600792bf6
RameesRoshen <ramizroshen4510@gamil.com>
2024-01-31 11:23:31 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Project from './pages/Project';
+
+import Dashboard from './pages/Dashboard';
+
+import Footer from './components/Footer';
+import Auth from './components/Auth';
+import { useContext } from 'react';
+import { isAuthTokenContext } from './Contexts/ContextShare';
+
 
 function App() {
+
+  const {isAuthToken,setIsAuthToken}=useContext(isAuthTokenContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+
+      <Route path='/login' element={<Auth/>} />
+
+      <Route path='/register' element={<Auth register/>} />
+
+      <Route path='/project' element={<Project/>} />
+
+      <Route path='/dashboard' element={ isAuthToken?<Dashboard Dashboard/>:<Home/> } />
+    </Routes>
+
+    <Footer/>
     </div>
   );
 }
